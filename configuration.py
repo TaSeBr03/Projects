fca7d684aa4b007994c469a7efb6e98cea28375e
Tatyana Bronman <taceta@yandex.ru>
2024-09-27 15:09:06 +0500

    My

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://3f8b181e-4246-4788-8d84-91f6fac4173e.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Тося",
+    "lastName": "Попова",
+    "address": "Тверская, 56",
+    "metroStation": 4,
+    "phone": "+79127732439",
+    "rentTime": 3,
+    "deliveryDate": "2024-09-27",
+    "comment": "Жду",
+    "color":[
+        "BLACK"
+    ]
+}
